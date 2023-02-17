a0f9a4e68a3943ddd51d2ba8063f660d4d31faa8
jventura7 <jpventura925@gmail.com>
2023-02-17 15:40:40 -0500

    added info for pages

---
+++ src/components/HeaderInfo.jsx
@@ -0,0 +1,72 @@
+import { motion } from "framer-motion";
+
+import stock1 from "../assets/images/stock1.jpg";
+import stock2 from "../assets/images/stock2.jpg";
+import stock3 from "../assets/images/stock3.jpg";
+import stock4 from "../assets/images/stock4.jpg";
+
+import { CheckArrowIcon } from "../assets/icons/CheckArrowIcon";
+
+export const HeaderInfo = ({ info }) => {
+  return (
+    <section
+      className="w-full bg-customDarkBg2 mt-20 mb-8 sm:mt-16 sm:mb-16 xl:mt-0  xl:m pt-[2rem]  md:pt-[12vw] lg:pt-0"
+      id="about"
+    >
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        <div className="flex flex-wrap items-center 2xl:w-[1450px] xl:w-[1300px] w-11/12 mx-auto md:pl-4 xl:pr-16 xl:pl-16">
+          <div className="w-full lg:w-1/2 mb-12 lg:mb-0">
+            <div className="mx-auto lg:mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-unset">
+              <span className="custom-block-subtitle">{ info.title }</span>
+              <h2 className="mt-6 mb-8 text-4xl lg:text-5xl custom-block-big-title">
+                { info.aboutTitle }
+              </h2>
+              <p className="mb-10 text-customGrayText leading-loose">
+                { info.about }
+              </p>
+            </div>
+          </div>
+          <div className="w-3/4 mx-auto lg:w-1/2 flex flex-wrap lg:-mx-4 sm:pr-8 lg:pt-10 justify-center lg:pl-4 xl:px-8">
+            <div className="mb-8 lg:mb-0 w-full sm:w-1/2 px-2 lg:px-0">
+              <div className="mb-4 py-3 pl-3 pr-2 rounded">
+                <img
+                  src={stock1}
+                  alt="s1"
+                  className="rounded-xl  custom-border-gray mx-auto sm:mx-unset"
+                />
+              </div>
+              <div className="py-3 pl-3 pr-2 rounded ">
+                <img
+                  src={stock2}
+                  alt="s2"
+                  className="rounded-xl  custom-border-gray mx-auto sm:mx-unset"
+                />
+              </div>
+            </div>
+            <div className="w-1/2 lg:mt-20  pt-12 lg:pt-0 px-2 hidden sm:inline-block">
+              <div className="mb-4 py-3 pl-3 pr-2 rounded-lg ">
+                <img
+                  src={stock3}
+                  alt="s3"
+                  className="rounded-xl  custom-border-gray"
+                />
+              </div>
+              <div className="py-3 pl-3 pr-2 rounded-lg ">
+                <img
+                  src={stock4}
+                  alt="s4"
+                  className="rounded-xl  custom-border-gray"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
