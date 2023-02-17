e3cf5240caf4b2d52485fbcfd9576015db27e38c
William Ventura <wventur1@jh.edu>
2023-02-17 16:35:50 -0500

    fixed astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
-  site: "https://wvent777.github.io"
+  integrations: [react(), tailwind()],
+  site: "https://wvent777.github.io",
+  base: '/ukrg_v2'
 });
